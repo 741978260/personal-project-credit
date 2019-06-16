29fa85e39a483945f3c128f861f4bc2dbb62161b
cailin <741978260@qq.com>
2019-06-16 20:45:31 +0800

    授信服务初始化

--- credit-server-webapp/src/main/java/com/mumu/customercenter/CreditServerWebappApplication.java
+++ credit-server-webapp/src/main/java/com/mumu/customercenter/CreditServerWebappApplication.java
@@ -2,8 +2,11 @@ package com.mumu.customercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CreditServerWebappApplication {
 
     public static void main(String[] args) {
